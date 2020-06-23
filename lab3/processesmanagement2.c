650bba6d81e5f1020fb556702aaa8b1e6647d521
Wiggidy <ronaldriggles@riseup.net>
2020-06-23 14:21:13 -0500

    OMAP

--- lab3/processesmanagement2.c
+++ lab3/processesmanagement2.c
@@ -328,12 +328,10 @@ void LongtermScheduler(void){
       EnqueueProcess(JOBQUEUE,currentProcess);
     }
     if (currentProcess->ProcessID == lastProcess->ProcessID) { // short circuit after the final process
-      printf("Reached Final Process in LTS!\n");
       break;
     }
     currentProcess = DequeueProcess(JOBQUEUE);
   }
-  printf("Leaving LTS!\n");
 }
 
 
