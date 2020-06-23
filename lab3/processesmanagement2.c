d480370a3637a7e6d3b0ae0294711b35c6698fe2
Wiggidy <ronaldriggles@riseup.net>
2020-06-23 15:20:48 -0500

    test

--- lab3/processesmanagement2.c
+++ lab3/processesmanagement2.c
@@ -20,7 +20,7 @@
 
 typedef enum {TAT,RT,CBT,THGT,WT,AWTJQ} Metric;
 
-typedef enum {TRUE,FALSE} Boolean;
+typedef enum {T,F} Boolean;
 
 /*****************************************************************************\
 *                             Global definitions                              *
@@ -48,7 +48,7 @@ typedef enum {TRUE,FALSE} Boolean;
 Quantity NumberofJobs[MAXMETRICS]; // Number of Jobs for which metric was collected
 Average  SumMetrics[MAXMETRICS]; // Sum for each Metrics
 
-// Boolean  MemoryTable[AvailableMemory]; // Table of available memory holes
+Boolean* MemoryTable = NULL;
 
 /*****************************************************************************\
 *                               Function prototypes                           *
