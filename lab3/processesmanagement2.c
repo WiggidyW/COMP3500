18f07fde53daa825be1a3873d9ab2eecd44b5331
Wiggidy <ronaldriggles@riseup.net>
2020-06-23 18:47:03 -0500

    test

--- lab3/processesmanagement2.c
+++ lab3/processesmanagement2.c
@@ -394,6 +394,8 @@ void LongtermScheduler(void){
   ProcessControlBlock *lastProcess = Queues[JOBQUEUE].Head; // tracks the final process in the queue
   ProcessControlBlock *currentProcess = DequeueProcess(JOBQUEUE);
 
+  if (currentProcess->ProcessID == 249) { BookKeeping(); } // fix corrupted size bug
+
   while (currentProcess) {
     currentProcess->TopOfMemory = *TableSize; // initialize the top of memory for the process
 
